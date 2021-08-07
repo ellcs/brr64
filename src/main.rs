ff9128748920073d0d56bb0d306717af0231c1ab
elx <me@ellcs.de>
2021-08-07 23:48:28 +0200

    Move symbolic code impl into new module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod playgroud;
 mod base64_encode;
+mod symbolic_base_bro;
 
+use regex::Regex;
 use std::env;
 use std::io;
 
@@ -18,8 +20,6 @@ fn main() {
     }
 }
 
-//fn base64_encode_symbolic(input: &String) -> (Vec<u8>, Vec<u8>, Vec<u8>) {
-//fn base64_encode_symbolic(input: &Bytes) -> Bytes {
 fn base64_encode_symbolic(input: Vec<u8>) -> Vec<Vec<u8>> {
     let mut input0: Vec<u8> = input.clone();
     let mut input1: Vec<u8> = input.clone();
@@ -49,107 +49,6 @@ fn base64_encode_symbolic(input: Vec<u8>) -> Vec<Vec<u8>> {
     outputs[1].splice(0..1, std::iter::repeat(b'=').take(1).collect::<Vec<u8>>());
     outputs[2].splice(0..2, std::iter::repeat(b'=').take(2).collect::<Vec<u8>>());
     outputs
-}
-
-enum Char64 {
-    Sym,
-    Real(u32)
-}
-
-fn base64_three_chars_symbolic(chars: &[Char64], result: &mut Vec<u8>) {
-    match chars {
-        [Char64::Real(c1), Char64::Real(c2), Char64::Real(c3)] => first_case(c1, c2, c3, result),
-        [Char64::Sym,      Char64::Real(c2), Char64::Real(c3)] => second_case(c2, c3, result),
-        [Char64::Sym,      Char64::Sym,      Char64::Real(c3)] => third_case(c3, result),
-        [Char64::Real(c1), Char64::Sym,      Char64::Sym]      => fourth_case(c1, result),
-        [Char64::Real(c1), Char64::Real(c2), Char64::Sym]      => fifth_case(c1, c2, result),
-        _ => panic!()
-    }
-}
-
-#[inline(always)]
-fn first_case(c1: &u32, c2: &u32, c3: &u32, result: &mut Vec<u8>) {
-    let group24: u32 = (c1 << 16) | (c2 << 8) | c3 ;
-    result[0] = base64_encode::BASE64_CHARS[((group24 >> 18) & 0x3f) as usize];
-    result[1] = base64_encode::BASE64_CHARS[((group24 >> 12) & 0x3f) as usize];
-    result[2] = base64_encode::BASE64_CHARS[((group24 >> 6) & 0x3f) as usize];
-    result[3] = base64_encode::BASE64_CHARS[(group24 & 0x3f) as usize];
-}
-
-#[inline(always)]
-fn second_case(c2: &u32, c3: &u32, result: &mut Vec<u8>) {
-    let group24: u32 = (0_u32 << 16) | (c2 << 8) | c3;
-    result[0] = base64_encode::EQUALS;
-    result[1] = base64_encode::BASE64_CHARS[((group24 >> 12) & 0x3f) as usize];
-    result[2] = base64_encode::BASE64_CHARS[((group24 >> 6) & 0x3f) as usize];
-    result[3] = base64_encode::BASE64_CHARS[(group24 & 0x3f) as usize];
-}
-
-#[inline(always)]
-fn third_case(c3: &u32, result: &mut Vec<u8>) {
-    let group24: u32 = (0_u32 << 16) | (0_u32 << 8) | c3;
-    result[0] = base64_encode::EQUALS;
-    result[1] = base64_encode::EQUALS;
-    result[2] = base64_encode::BASE64_CHARS[((group24 >> 6) & 0x3f) as usize];
-    result[3] = base64_encode::BASE64_CHARS[(group24 & 0x3f) as usize];
-}
-
-#[inline(always)]
-fn fourth_case(c3: &u32, result: &mut Vec<u8>) {
-    let group24: u32 = (c3 << 16) | (0_u32 << 8) | 0_u32;
-    result[0] = base64_encode::BASE64_CHARS[((group24 >> 18) & 0x3f) as usize];
-    result[1] = base64_encode::BASE64_CHARS[((group24 >> 12) & 0x3f) as usize];
-    result[2] = base64_encode::EQUALS;
-    result[3] = base64_encode::EQUALS;        
-}
-
-#[inline(always)]
-fn fifth_case(c1: &u32, c2: &u32, result: &mut Vec<u8>) {
-    let group24: u32 = (c1 << 16) | (c2 << 8) | 0_u32;
-    result[0] = base64_encode::BASE64_CHARS[((group24 >> 18) & 0x3f) as usize];
-    result[1] = base64_encode::BASE64_CHARS[((group24 >> 12) & 0x3f) as usize];
-    result[2] = base64_encode::BASE64_CHARS[((group24 >> 6) & 0x3f) as usize];
-    result[3] = base64_encode::EQUALS;        
-}
-
-#[test]
-fn base64_three_chars_symbolic_first_case() {
-    let array = [Char64::Real(0), Char64::Real(1), Char64::Real(3)];
-    let mut output: Vec<u8> = vec![0, 0, 0, 0];
-    base64_three_chars_symbolic(&array, &mut output);
-    assert_eq!(output, vec![65, 65, 69, 68]);
-}
-
-#[test]
-fn base64_three_chars_symbolic_second_case() {
-    let array = [Char64::Sym, Char64::Real(1), Char64::Real(3)];
-    let mut output: Vec<u8> = vec![0, 0, 0, 0];
-    base64_three_chars_symbolic(&array, &mut output);
-    assert_eq!(output, vec![61, 65, 69, 68]);
-}
-
-#[test]
-fn base64_three_chars_symbolic_third_case() {
-    let array = [Char64::Sym, Char64::Sym, Char64::Real(3)];
-    let mut output: Vec<u8> = vec![0, 0, 0, 0];
-    base64_three_chars_symbolic(&array, &mut output);
-    assert_eq!(output, vec![61, 61, 65, 68]);
-}
-
-#[test]
-fn base64_three_chars_symbolic_fourth_case() {
-    let array = [Char64::Real(0), Char64::Sym, Char64::Sym];
-    let mut output: Vec<u8> = vec![0, 0, 0, 0];
-    base64_three_chars_symbolic(&array, &mut output);
-    assert_eq!(output, vec![65, 65, 61, 61]);
-}
-
-#[test]
-fn base64_three_chars_symbolic_fifth_case() {
-    let array = [Char64::Real(0), Char64::Real(1), Char64::Sym];
-    let mut output: Vec<u8> = vec![0, 0, 0, 0];
-    base64_three_chars_symbolic(&array, &mut output);
-    assert_eq!(output, vec![65, 65, 69, 61]);
 }
 
 fn symbolic_test(input: &[u8], vecs: Vec<&[u8]>) {
