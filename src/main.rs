5df56a91e4314fe1e48161634777cc801f7be5c7
elx <me@ellcs.de>
2021-08-31 00:14:30 +0200

    Open stdin or file

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,39 @@ pub fn options() -> args::Options {
     options
 }
 
+pub fn open_io(options: &args::Options) -> Box<dyn std::io::Read + 'static> {
+    let path = options.input_file.clone();
+    if path.as_os_str() == "-" {
+        Box::new(std::io::stdin())
+    } else {
+        match std::fs::File::open(&path) {
+            Ok(file) => Box::new(file),
+            Err(_err) => {
+                eprintln!("Could not open file: {}", path.display());
+                std::process::exit(1);
+            }
+        }
+    }
+}
 
 fn main() {
     let options = options();
     let candidates = symbolic_base_bro::generate_candidates(&options.search_term);
     let regex = convert::regex_string_by_candidates(&candidates, &options.convert_options);
 
-    println!("{}", regex);
+    
+    
+    let buf = open_io(&options);
+    let mut buffer = [0; 5];
+
+    // read at most five bytes
+    let mut handle = buf.take(5);
+
+    handle.read(&mut buffer).unwrap();
+
+    println!("{:?}", buffer);
+
+    if options.print_regex {
+        println!("{}", regex);
+    }
 }
