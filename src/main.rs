be08191a88cd43cf27cc3168cf5fb129c4f373ef
lex <me@ellcs.de>
2021-10-08 11:13:17 +0200

    Introduce log

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use brr64::search;
 
 use std::io::BufRead;
 use std::io::Read;
+
+use log::{debug, error, info, warn};
 use structopt::StructOpt;
 use atty::Stream;
 
@@ -14,41 +16,6 @@ pub fn options() -> args::Options {
     options.stdin_tty = atty::is(Stream::Stdin);
     options.stdout_tty = atty::is(Stream::Stdout);
     options
-}
-
-pub fn find_in_stream<R: Read>(mut rdr: R, candidates: &symbolic_base_bro::Candidates) {
-    const BUFFER_SIZE: usize = 1 << 8;
-
-    let candidates = symbolic_base_bro::generate_candidates("asdf");
-    let mut search = search::by_candidates(&candidates);
-    let mut buffer_vec = Vec::with_capacity(BUFFER_SIZE);
-    let mut operation = |bytes: &[u8]| {
-        if search::push_all(&mut search, bytes) {
-            let found = search.search_stack.iter().find(|s| {
-                s.current_candidate.is_empty()
-            });
-            println!("{}", bytes.len());
-        }
-    };
-    loop {
-        match rdr.by_ref().take(BUFFER_SIZE as u64).read_to_end(&mut buffer_vec) {
-            Err(err) => {
-                eprintln!("Error while reading file: {}", err);
-                std::process::exit(1);
-            },
-            Ok(chunk_size) => {
-                if chunk_size == 0 {
-                    break;
-                }
-                operation(&buffer_vec);
-                if chunk_size < BUFFER_SIZE {
-                    break;
-                }
-                buffer_vec.clear();
-            }
-        }
-
-    }
 }
 
 
@@ -62,14 +29,14 @@ fn main() {
     } else {
         let path = options.input_file.clone();
         if path.as_os_str() == "-" {
-            find_in_stream(std::io::stdin(), &candidates);
+            search::find_in_stream(std::io::stdin(), &candidates);
         } else {
             match std::fs::File::open(&path) {
                 Ok(file) => {
-                    find_in_stream(file, &candidates);
+                    search::find_in_stream(file, &candidates);
                 },
                 Err(_err) => {
-                    eprintln!("Could not open file: {}", path.display());
+                    error!("Could not open file: {}", path.display());
                     std::process::exit(1);
                 }
             }
