6dc0251513929358f0fc8cce5327f1fbf8a0849d
elx <me@ellcs.de>
2022-01-26 21:57:48 +0100

    Use named parameter in proxy-struct

--- brr64-wasm/src/lib.rs
+++ brr64-wasm/src/lib.rs
@@ -12,44 +12,40 @@ use brr64::convert;
 use brr64::args;
 
 use wasm_bindgen::prelude::*;
+//use js_sys::{Function, Object, Reflect, WebAssembly};
 
 #[derive(Clone)]
 #[wasm_bindgen(js_name = ConvertOptions)]
-pub struct ConvertOptionsWrapper(args::ConvertOptions);
+pub struct ConvertOptionsWrapper { 
+    wrapped: args::ConvertOptions
+}
 
 #[wasm_bindgen(js_class = ConvertOptions)]
 impl ConvertOptionsWrapper {
 
 	#[wasm_bindgen(constructor)]
 	pub fn new() -> ConvertOptionsWrapper {
-		ConvertOptionsWrapper(args::ConvertOptions { dont_match_newlines: false, print_equals: false })
+		ConvertOptionsWrapper {
+            wrapped: args::ConvertOptions { dont_match_newlines: false, print_equals: false }
+        }
 	}
 
     #[wasm_bindgen(setter)]
     pub fn set_dont_match_newlines(&mut self, value: bool) {
-        self.0.dont_match_newlines = value;
+        self.wrapped.dont_match_newlines = value;
     }
 
     #[wasm_bindgen(setter)]
     pub fn set_print_equals(&mut self, value: bool) {
-        self.0.print_equals = value;
+        self.wrapped.print_equals = value;
     }
+
 }
 
 
 #[wasm_bindgen]
 pub fn candidates(input: String, options: &ConvertOptionsWrapper) -> String {
     let candidates = symbolic_base_bro::generate_candidates(&input);
-    convert::regex_string_by_candidates(&candidates, &options.clone().0)
+    convert::regex_string_by_candidates(&candidates, &options.wrapped)
 }
 
-#[wasm_bindgen]
-extern "C" {
-        fn alert(s: &str);
-}
-
-
-#[wasm_bindgen]
-pub fn run_alert(item: &str) {
-        alert(&format!("This is WASM and {}", item));
-}
