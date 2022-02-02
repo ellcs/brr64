154a52bbf7690852693bfe11fabb8cddafe6a743
elx <me@ellcs.de>
2022-02-02 23:42:23 +0100

    Remove sh

--- sh/runner_github_action.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pushd /code
-bash sh/rustup.sh
-bash sh/tests.sh
-bash sh/build_wasm.sh
-popd
--- sh/tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PARENT_DIR="$(dirname "$(realpath "$0")")"
-source "${PARENT_DIR}/common.sh"
-
-exec_and_echo "cargo test --verbose"
